9de409b803b27ee32c7cec36f41f15718ab56d15
Baltinha <63794832+Baltinha@users.noreply.github.com>
2024-09-07 23:28:39 -0300

    Finalizado.

--- Test-TGID/src/main/java/MeuPacote/Empresas.java
+++ Test-TGID/src/main/java/MeuPacote/Empresas.java
@@ -9,18 +9,25 @@ package MeuPacote;
  * @author nomed
  */
 public class Empresas {
-    
-    private static int num;
-    private static int saldo;
-    private static String CNPJ;
-    private static float taxa;
+    //Classe para moldar uma Empresa
+    private  int num;
+    private  double saldo;
+    private  String CNPJ;
+    private  double taxa;
     
     public int getNum ()
     {
+        //pegar o valor do numero da empresa
         return this.num;
     }
-    public Empresas(int num, String CNPJ,float taxa,int saldo)
+    public String getCNPJ ()
     {
+        //pegar o valor do CNPJ
+        return this.CNPJ;
+    }
+    public Empresas(int num, String CNPJ,double taxa,int saldo)
+    {
+        //passagem de valores para a montagem da empresa
        this.num = num;
        this.saldo = saldo;
        this.CNPJ = CNPJ;
@@ -29,7 +36,7 @@ public class Empresas {
     
     public void printEmpresa(){
         System.out.println("\tEmpresas");
-        System.out.println("Numero da Empresa: "+ num);
+        System.out.println("ID da Empresa: "+ num);
         System.out.println("Saldo atual:" + saldo);
         System.out.println("CNPJ: " + (CNPJ.substring(0, 2) + "." + CNPJ.substring(2, 5) + "." +
       CNPJ.substring(5, 8) + "." + CNPJ.substring(8, 12) + "-" +
@@ -37,12 +44,20 @@ public class Empresas {
         
     }
     public void sacar(double valor){
+        if (saldo < valor) {
+            System.out.println("Saldo da empresa e isuficiente para saque!");
+            return;
+        }
+        
+        saldo -=valor-taxa;
+        System.out.println("Saque bem sucedido no valor: R$"+valor+" com taxa: R$"+taxa);
+ 
+    }
 
-}
-
-    public void depositar(double valor)
-    {
-
+    public  void depositar(double valor)
+    {     
+        saldo +=valor;
+        System.out.println("desposito no valor: R$"+ valor); 
     }
 
 
--- Test-TGID/src/main/java/com/mycompany/test/tgid/TestTGID.java
+++ Test-TGID/src/main/java/com/mycompany/test/tgid/TestTGID.java
@@ -13,74 +13,92 @@ import MeuPacote.Empresas;
  * @author nomed
  */
 public class TestTGID {
-    
-    static int index =0; 
+
+    static int index = 0;
     static Empresas[] empresas = new Empresas[5];
     static Scanner ler = new Scanner(System.in);
-    
+
     public static void main(String[] args) {
 
-        
         SistemaInicial();
 
     }
-    public static void SistemaInicial()
-    {
+
+    public static void SistemaInicial() {
         System.out.println("\tCliente digite 1");
         System.out.println("\tEmpresa digite 2");
         System.out.println("\tMostrar Empresas digeite 3");
         System.out.println("\tFechar digeite 4");
         System.out.print("Opcao: ");
-        int sistema = ler.nextInt();    
+        int sistema = ler.nextInt();
         Sistema(sistema);
-        
+
     }
-        public static void escolheOpcao(int opcao){
 
-            switch( opcao ){
-                case 1: 
-                    System.out.println("Saque");
-
-
-                case 2:
-                    System.out.println("deposito");
-
-
-                case 3:
-                        SistemaInicial();
-                        break;
-                default:
-                        System.out.println("Opção inválida");
-            }
-    }
     public static void SistemaCliente() {
 
         System.out.println("\t Escolha a opcao desejada");
         System.out.println("1 - Sacar");
         System.out.println("2 - Depositar");
         System.out.println("3 - Voltar\n");
-        System.out.print("Opcao: ");  
- 
+        System.out.print("Opcao: ");
+
     }
-    public static void Sistema(int sistema)
-    {
-        
+
+    public static void escolheOpcao(int opcao) {
+        //Opção do Cliente para usar na Empresa
+        switch (opcao) {
+            case 1:
+                //Opção do Cliente para Sacar dinheiro da empresa com taxa
+                System.out.println("Digite o CNPJ da empresa que deseja sacar? ");
+                String tempCNPJ = ler.next();
+                Empresas empresaTemp = selecionaEmpresa(tempCNPJ);
+                if (empresaTemp == null) {
+                    System.out.println("Nao existe uma empresa com o CNPJ ecolhido!");
+                    escolheOpcao(1);
+                }
+                System.out.println("Quanto voce quer sacar? ");
+                double saque = ler.nextDouble();
+                empresaTemp.sacar(saque);
+                break;
+            case 2:
+                //Opção do Cliente para depositar na empresa
+                System.out.println("Digite o CNPJ da empresa que deseja depositar?");
+                tempCNPJ = ler.next();
+                empresaTemp = selecionaEmpresa(tempCNPJ);
+                if (empresaTemp == null) {
+                    System.out.println("Não existe uma empresa com o CNPJ ecolhido!");
+                    escolheOpcao(2);
+                }
+                System.out.println("Quanto voce quer depositar? ");
+                double deposito = ler.nextDouble();
+                empresaTemp.depositar(deposito);
+                break;
+            case 3:
+                //volta pra o inicio do programa
+                SistemaInicial();
+                break;
+            default:
+                System.out.println("Opção inválida");
+        }
+        SistemaInicial();
+    }
+
+    public static void Sistema(int sistema) {
+
         int num, saldo;
         String CPF;
         String CNPJ;
         float taxa;
-        
-        
 
-               switch(sistema)
-        {
+        switch (sistema) {
             case 1:
                 System.out.println("Informe o CPF: ");
-                    CPF = ler.next();
+                CPF = ler.next();
                 if (ValidaCPF.isCPF(CPF) == true) {
-                SistemaCliente();
-                int clienteTemp = ler.nextInt();
-                escolheOpcao(clienteTemp);
+                    SistemaCliente();
+                    int clienteTemp = ler.nextInt();
+                    escolheOpcao(clienteTemp);
                 } else {
                     System.out.printf("Erro, CPF invalido !!!\n");
                 }
@@ -91,62 +109,71 @@ public class TestTGID {
                 System.out.println("Informe o CNPJ: ");
                 CNPJ = ler.next();
                 if (ValidaCNPJ.isCNPJ(CNPJ) == true) {
-                System.out.println("Informe o numero da empresa: ");
-                num = ler.nextInt();
-                System.out.println("Infome a taxa: ");
-                taxa = ler.nextFloat();
-                System.out.println("Infome a saldo: ");
-                saldo = ler.nextInt();
-                
-                FazerEmpresas(num,CNPJ,taxa,saldo);
-                System.out.println("Quer colocar mais uma empresa");
-                System.out.print(" 1 pra sim e 2 pra nao: ");
-                int empresasTemp = ler.nextInt();
+                    System.out.println("Informe o ID da empresa: ");
+                    num = ler.nextInt();
+                    System.out.println("Infome a taxa: ");
+                    taxa = ler.nextFloat();
+                    System.out.println("Infome a saldo: ");
+                    saldo = ler.nextInt();
+
+                    FazerEmpresas(num, CNPJ, taxa, saldo);
+                    System.out.println("Quer colocar mais uma empresa");
+                    System.out.print(" 1 pra sim e 2 pra nao: ");
+                    int empresasTemp = ler.nextInt();
                     if (empresasTemp == 1) {
                         Sistema(2);
-                    }else{SistemaInicial();}
+                    } else {
+                        SistemaInicial();
+                    }
+                } else {
+                    System.out.printf("Erro, CNPJ invalido !!!\n");
                 }
-                else System.out.printf("Erro, CNPJ invalido !!!\n");
                 break;
             case 3:
-
-            for (Empresas empresa : empresas) {
-                if (empresas[0] == null)
-                {
-                    System.out.println("Nao existem empresas cadastradas!");
-                    break;
+                //Mostra todas as impresas cadatradas no sistema
+                for (Empresas empresa : empresas) {
+                    if (empresas[0] == null) {
+                        System.out.println("Nao existem empresas cadastradas!");
+                        break;
+                    }
+                    if (empresa == null) {
+                        return;
+                    }
+                    empresa.printEmpresa();
                 }
-                if (empresa == null) {
-                    return;
-                }
-                empresa.printEmpresa();
-            }
 
                 break;
-
-                
+            default:
+                System.out.println("Opção inválida");
         }
-        SistemaInicial(); 
+        //volta pra o inicio
+        SistemaInicial();
     }
-    public static void adicionarEmpresa(Empresas empresa)
-    {
+
+    public static void adicionarEmpresa(Empresas empresa) {
         if (index < 5) {
             empresas[index] = empresa;
-            index = index+1;
+            index = index + 1;
         }
     }
-    public static void FazerEmpresas(int num, String CNPJ, float taxa,int saldo)
-    {
-        Empresas novaEmpresa = new Empresas(num, CNPJ,taxa, saldo);
+
+    public static void FazerEmpresas(int num, String CNPJ, float taxa, int saldo) {
+        Empresas novaEmpresa = new Empresas(num, CNPJ, taxa, saldo);
         adicionarEmpresa(novaEmpresa);
-        /*for (int i = 0; i < empresas.length; i++) {
-            if (empresas[i] != null) {
-                break;
+
+    }
+
+    public static Empresas selecionaEmpresa(String CNPJ) {
+
+        Empresas empresaSelecionada = null;
+        for (int i = 0; i < empresas.length; i++) {
+
+            if (empresas[i] != null && empresas[i].getCNPJ().equals(CNPJ)) {
+                empresaSelecionada = empresas[i];
+                return empresaSelecionada;
+
             }
-            empresas[i] = novaEmpresa;
- 
-        }*/
-        
+        }
+        return empresaSelecionada;
     }
 }
-
